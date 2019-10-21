1fca73cc276ac648a22b0b9f84143ad02baf3d9d
Adri Dutta <adridutta@utexas.edu>
2019-10-21 16:47:17 -0500

    removed password field from php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
   <?php
       $dsn = 'mysql:unix_socket=/cloudsql/backend-256601:us-central1:database;dbname=testdata';
       $user = 'duttaadri2014@gmail.com';
-      $password = 'D6qs1FPzuOdE6JFu';
-      $db = new PDO($dsn, $user, $password);
+      $db = new PDO($dsn, $user);
       $statement = "Select article_name FROM books";
       $statement->execute();
       $all = $statement->fetchAll();
