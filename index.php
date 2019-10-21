5c3c5df8ea8e103ee6a77681cc6add44416a143c
Adri Dutta <Duttaadri2014@gmail.com>
2019-10-21 18:24:06 -0500

    yaml

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
       $dsn = 'mysql:unix_socket=/cloudsql/backend-256601:us-central1:database;dbname=testdata';
       $user = 'duttaadri2014@gmail.com';
       $db = new PDO($dsn, $user);
-      $statement = "Select article_name FROM books";
+      echo "something is something";
+      $statement = $db->prepare("use books;");
+      $statement->execute();
+      $statement = $db->prepare("select article_name from books;");
       $statement->execute();
       $all = $statement->fetchAll();
-      echo "something is something";
-      foreach ($all as $data)
+      foreach ( $all as $data)
       {
-	echo $data["article_name"]."<br>";
-
+          echo $data['article_name'], '<br>';
       }
-	
    ?>
