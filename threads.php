8173d2bb3c6c9b3799832a37a234fac5fb37d631
AustinDuong <duongaustin97@gmail.com>
2019-10-22 17:27:41 -0500

    Linking php files working, about page script still not working

--- threads.php
+++ threads.php
@@ -1,5 +1,21 @@
-﻿<!DOCTYPE html>
+  <?php
+      $dsn = 'mysql:unix_socket=/cloudsql/backend-256601:us-central1:database;dbname=testdata';
+      $user = 'duttaadri2014@gmail.com';
+      $db = new PDO($dsn, $user);
+      echo "something is something";
+      $statement = $db->prepare("use books;");
+      $statement->execute();
+      $statement = $db->prepare("select article_name from books;");
+      $statement->execute();
+      $all = $statement->fetchAll();
+      foreach ( $all as $data)
+      {
+          echo $data['article_name'], '<br>';
+      }
+   ?>
 
+
+﻿<!DOCTYPE html
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
