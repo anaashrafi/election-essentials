9740209f960f3bcc953a4bb0fc8aa9b0cf2d8602
AustinDuong <Duong_Austin@utexas.edu>
2019-10-22 16:45:00 -0500

    Converted .html extensions to .php

--- main_static.php
+++ main_static.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
-
+   <!-- comment -->
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
     <title>Election Essentials</title>
-	<link href="user_profile_style.css" rel="stylesheet" type="text/css">
+	<link href="main_style.css" rel="stylesheet" type="text/css">
+  <script>
+    function submit() {
+      location.href = "threads.php";
+    }
+</script>
 </head>
 
 <body>
@@ -12,17 +17,12 @@
 
 		<div class ="table">
 			<ul class ="nav-tabs">
-				<li><a href="threads.html"> HOME </a></li> <!-- class="active-tab" means this is the page the user is currently on-->
-				<li><a href="candidates.html"> CANDIDATES </a></li>
-				<li><a href="voting_info.html"> VOTER INFORMATION </a></li>
-				<li class="active-tab"><a href="user_profile.html"> MY PROFILE </a></li>
-        <li><a href="login.html"> LOGIN </a></li>
-				<li><a href="aboutme.html"> ABOUT </a></li>
-			</ul>
-		</div>
+				<li class="active-tab"><a href="login.php"> LOGIN </a></li> <!-- class="active-tab" means this is the page the user is currently on-->
+          </ul>
+        </div>
 
-    <div id="essentialsForeground">
-        <h2 class="title"> Your Essentials </h2>
+      <div id="foreground">
+        <h2 id="title"> What Are Your Essentials? </h2>
             <div class="checkBoxWrapper" style="grid-row-start:2;grid-column-start:2;">
               <input class="checkBox" type="checkbox">
               <p class="checkBoxChoices"style="grid-column-start:2;"> Economy
@@ -83,25 +83,13 @@
               <p class="checkBoxChoices"style="grid-column-start:2;"> Gun Violence
 </p>
             </div>
-
+            <div style="grid-row-start:7;grid-column-start:2;grid-column-end:4;justify-content:center;padding-left:48%;transform:scale(1.5);">
+            <br>
+            <button onclick="submit()">SUBMIT</button>
+          </div>
 		</div>
 
-    <div id="bookmarksForeground">
-      <h2 class="title"> Bookmarks </h2>
-      <div class="bookmarkWrapper" style="grid-row-start:2;grid-column-start:2;">
 
-              <a class="bookmarkText" href="">Sample Bookmark #1</a>
-            </div>
-      <div class="bookmarkWrapper" style="grid-row-start:3;grid-column-start:2;">
-
-              <a class="bookmarkText" href="">Sample Bookmark #2</a>
-            </div>
-
-      <div class="bookmarkWrapper" style="grid-row-start:4;grid-column-start:2;">
-
-              <a class="bookmarkText" href="">Sample Bookmark #3</a>
-            </div>
-    </div>
 
 	</header>
 </body>
--- threads.php
+++ threads.php
@@ -12,25 +12,25 @@
 
 		<div class ="table">
 			<ul class ="nav-tabs">
-				<li class="active-tab"><a href="threads.html"> HOME </a></li> <!-- class="active-tab" means this is the page the user is currently on-->
-				<li><a href="candidates.html"> CANDIDATES </a></li>
-				<li><a href="voting_info.html"> VOTER INFORMATION </a></li>
-				<li><a href="user_profile.html"> MY PROFILE </a></li>
-				<li><a href="login.html"> LOGIN </a></li>
-				<li><a href="aboutme.html"> ABOUT </a></li>
+				<li class="active-tab"><a href="threads.php"> HOME </a></li> <!-- class="active-tab" means this is the page the user is currently on-->
+				<li><a href="candidates.php"> CANDIDATES </a></li>
+				<li><a href="voting_info.php"> VOTER INFORMATION </a></li>
+				<li><a href="user_profile.php"> MY PROFILE </a></li>
+				<li><a href="login.php"> LOGIN </a></li>
+				<li><a href="aboutme.php"> ABOUT </a></li>
 			</ul>
 		</div>
 
 		<!-- temporary "articles" that will link to expanded articles when clicked -->
 		<div class ="samplearticles">
 
-			<a href="expanded_article_1.html" class="article">
+			<a href="expanded_article_1.php" class="article">
 				<div class="article-name">Sample Article 1</div>
 			</a>
-			<a href="expanded_article_2.html" class="article">
+			<a href="expanded_article_2.php" class="article">
 				<div class="article-name">Sample Article 2</div>
 			</a>
-			<a href="expanded_article_3.html" class="article">
+			<a href="expanded_article_3.php" class="article">
 				<div class="article-name">Sample Article 3</div>
 			</a>
 		</div>
