20a54bf6542cb43b4b2b6717f9e657def201ff49
Adri Dutta <Duttaadri2014@gmail.com>
2019-10-23 09:43:11 -0500

    main page sends data to php file

---
+++ preference_send.php
@@ -0,0 +1,18 @@
+  <?php
+    
+    echo $_POST['economy'];
+    
+    //   $dsn = 'mysql:unix_socket=/cloudsql/backend-256601:us-central1:database;dbname=testdata';
+    //   $user = 'duttaadri2014@gmail.com';
+    //   $db = new PDO($dsn, $user);
+    //   $statement = $db->prepare("Select article_name FROM books;");
+    //   $statement->execute();
+    //   $all = $statement->fetchAll();
+    //   echo "something is something";
+    //   foreach ($all as $data)
+    //   {
+	// echo $data["article_name"]."<br>";
+
+    //   }
+	
+   ?>
