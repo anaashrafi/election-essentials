f79a4e69e32cfe235b2838060a7a331dba2b4033
Ana Ashrafi <ana.a.ashrafi@gmail.com>
2019-11-13 17:18:28 -0600

    Changed styling of user profile page

--- user_profile.php
+++ user_profile.php
@@ -5,6 +5,10 @@
         <meta charset='utf-8' />
         <title>Election Essentials</title>
         <link href='user_profile_style.css' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
         <?php 
             include 'login.php';
@@ -84,26 +88,33 @@
 ?>
 
     <body>
-        <header>
+    <div class="w3-top">
+        <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+            <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+            <a href="threads.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
+            <a href="candidates.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Candidates</a>
+            <a href="voting_info.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Voting Info</a>
+            <a href="user_profile.php" class="w3-bar-item w3-button w3-padding-large w3-white">My Profile</a>
+            <a href="aboutme.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">About</a>
+        </div></div>
+    <style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+</style>
+        <header class="w3-container w3-red w3-center" style="padding:128px 16px">
+            
+<h1 class="w3-margin w3-jumbo">ELECTION ESSENTIALS</h1>
+  <p class="w3-xlarge">What is essential to YOU?</p> </header>
+  <div class="w3-container w3-center w3-white w3-padding-16">
 
-            <div class ='table'>
-                <ul class ='nav-tabs'>
-                    <li><a href='threads.php'> HOME </a></li> <!-- class='active-tab' means this is the page the user is currently on-->
-                    <li><a href='candidates.php'> CANDIDATES </a></li>
-                    <li><a href='voting_info.php'> VOTER INFORMATION </a></li>
-                    <li class='active-tab'><a href='user_profile.php'> MY PROFILE </a></li>
-                    <li><a href='aboutme.php'> ABOUT </a></li>
-                </ul>
-            </div>
-
-        <div id='essentialsForeground'>
-            <h2 class='title'> Your Essentials </h2>
+            <h3 class='title'> Your Essentials </h3>
                 <div class="checkBoxWrapper" style="grid-row-start:2;grid-column-start:2;">
               <input id = "jobs" class="checkBox" type="checkbox" <?php if($jobs){echo "checked";}?>>
               <p class="checkBoxChoices"style="grid-column-start:2;"> Jobs/Wages
 </p>
             </div>
-
+        
             <div class="checkBoxWrapper" style="grid-row-start:2;grid-column-start:3;">
               <input id = "environment"  class="checkBox" type="checkbox" <?php if($environment){echo "checked";}?>>
               <p class="checkBoxChoices" style="grid-column-start:2;"> Environment
@@ -162,10 +173,10 @@
             <br>
             <button id="submit">SUBMIT</button>
           </div>
-            </div>
+            </div></div>
 
-        <div id='bookmarksForeground'>
-        <h2 class='title'> Bookmarks </h2>
+        <div class="w3-container w3-center w3-light-grey w3-padding-16">
+        <h3 class='title'> Bookmarks </h3>
 <?php
     function escape($value){
         $search = array("'",  '"');
@@ -189,14 +200,14 @@
     for($i = 0; $i < $rows; $i++){
         $escTitle = escape($bookmarks[$i]['Article']);
         echo "
-            <div class='bookmarkWrapper' style='grid-row-start:".($i+2).";grid-column-start:1;'>
+            <div class='w3-panel w3-white w3-round-xlarge'>
                 <a class='bookmarkText' href='expanded_article_1.php?title=".$escTitle."'>".$bookmarks[$i]['Article']."</a>
-                <button onclick='removeBookmark(\"".$escTitle."\")' class='removeButton'>Remove</button>
+                <button onclick='removeBookmark(\"".$escTitle."\")' class='w3-button w3-black w3-padding-large w3-large w3-margin-top'>Remove</button>
             </div>
         ";
     }
 ?>
 
-        </header>
+        
     </body>
     </html>
