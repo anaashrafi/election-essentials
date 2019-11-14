06922733bf861c3baabd060da2406657c213f8a5
RoyceHong <Rhchen73@utexas.edu>
2019-11-14 17:26:46 -0600

    updated voting_info.php

--- voting_info.php
+++ voting_info.php
@@ -1,33 +1,40 @@
-<?php
+<?php 
     session_start();
-        $dsn = 'mysql:unix_socket=/cloudsql/backend-256601:us-central1:database;dbname=testdata';
-        $user = 'duttaadri2014@gmail.com';
-        $db = new PDO($dsn, $user);
-        $statement = $db->prepare("use Election_Essentials;");
-        $statement->execute();
-        $sqlGet = "Select * from Vo_In;";
-        $statement = $db->prepare($sqlGet);
-        $statement->execute();
-        $data = $statement->fetchAll();
-        $general = $data[0]['Data1'];
-        $primary = $data[1]['Data1'];
-
-
-
-
-
-
 ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'> </script> 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<script type='text/javascript'>
+
+    function getVotingInfo(){
+        
+        var obj = $.getJSON("voting_info.json",
+            function(json) {
+                //TODO
+                var voting_data = json;
+                var output = '<ul id="myUL">';
+                $(voting_data).each(function() {
+                    output += '<li><a href="#"><b>' + this.State + '</b><br>Primary Election Deadline: '
+                    + this.PrimaryElectionDeadline + '<br>General Election Deadline: ' + 
+                    this.GeneralElectionDeadline + '</li>';
+                    
+                }
+                );
+                output += '</ul>';
+                document.getElementById('state-dates').innerHTML=output;        
+            });
+        }
+</script>
+
 <head>
     <meta charset="utf-8" />
     <title>Election Essentials</title>
 	<link href="voting_info_style.css" rel="stylesheet" type="text/css">
 </head>
 
-<body>
+<body onload='getVotingInfo()'>
 	<header>
     <script>
         function noAccess(){
@@ -51,23 +58,11 @@
   			</ul>
           </div>
 </header>
-          <?php include 'login.php';?>
+<?php include 'login.php';?>
 
 <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for state info...">
 
-<ul id="myUL">
-  <li><a href="#">Alabama</a></li>
-  <li><a href="#">Alaska</a></li>
-  <li><a href="#">Arizona</a></li>
-  <li><a href="#">Arkansas</a></li>
-
-  <li><a href="#">California</a></li>
-  <li><a href="#">Colorado</a></li>
-  <li><a href="#">Connecticut</a></li>
-
-  <li><a href="#">Delaware</a></li>
-  
-</ul>
+<div id = 'state-dates'></div>
 
 
 
@@ -75,7 +70,6 @@
 
 
 </html>
-'
 
 <script>
 function myFunction() {
