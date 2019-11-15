138e0806cdec6750ff88c4c9ad3a3e9d78f053a6
Phat Le <phatle10@gmail.com>
2019-11-14 21:05:55 -0600

    fixed anon about me to profile page bug

--- aboutme.php
+++ aboutme.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'> </script> 
     <html lang='en' xmlns='http://www.w3.org/1999/xhtml'>
