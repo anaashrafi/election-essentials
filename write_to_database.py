c61825d90aa9f6ac7d7874ad1b250672cc52d6ae
RoyceHong <Rhchen73@utexas.edu>
2019-11-24 19:34:48 -0600

    Refactored backend

--- write_to_database.py
+++ write_to_database.py
@@ -1,79 +1,76 @@
 # Python code to illustrate and create a
 # table in database
+
+import urllib.request, json
+from datetime import date
+
 import sys
 sys.path.insert(0, 'lib')
 
 import mysql.connector
 from mysql.connector import errorcode
 
-import json
 from pprint import pprint
 
 import newspaper
 from newspaper import Article
 
-import urllib
 
-dict = {}
+topics = {"Jobs": "jobs",
+        "Healthcare": "healthcare",
+        "CJS": "criminal+justice+system",
+        "Environment": "environment",
+        "Education": "education",
+        "Gun Violence": "+gun+violence",
+        "Immigration": "immigration",
+        "Taxes": "taxes",
+        "LGBTQ": "lgbtq",
+        "Reproductive Issues": "abortion"}
+
+dataList = {}
+
+for topic in topics.keys():
+    getUrl = "https://newsapi.org/v2/everything?q=" + topics[topic] + "&language=en&sortBy=relevancy&apiKey=7ed7b7405fa84630879ef25babb23b1a"
+    with urllib.request.urlopen(getUrl) as url:
+        data = json.loads(url.read().decode())
+        dataList[topic] = data
+
+duplicate = {}
+
+#Connect to database
+print('\nConnecting to database')
 try:
     cnx = mysql.connector.connect(user='root', host='35.188.54.9', password='D6qs1FPzuOdE6JFu', database='Election_Essentials')
-    #cnx = mysql.connector.connect(user='root', host='35.223.188.180', password='123456', database='guestbook')
-    #sql_select_Query = "SELECT * from tasks"
-    #sql_update_Query = ("UPDATE article" "SET title = %s, body = %s")
     sql_insert_Query = ("INSERT INTO Ar_Ti_To_Ar" "(Title,Article)" "VALUES(%s,%s)")
     sql_insert_essential_Query = ("INSERT INTO Es_To_Ar_Ti" "(Essential,Title1)" "VALUES(%s,%s)")
     sql_delete_Query = ("truncate table Ar_Ti_To_Ar")
     cursor = cnx.cursor()
-    #cursor.execute(sql_select_Query)
-    #records = cursor.fetchall()
     cursor.execute(sql_delete_Query)
     sql_delete_Query = ("truncate table Es_To_Ar_Ti")
     cursor.execute(sql_delete_Query)
 
-
-    #Read from json file
-
-    topics = ["Taxes",
-            "Healthcare",
-            "CJS",
-            "Jobs",
-            "Environment",
-            "Education",
-            "Gun Violence",
-            "Immigration",
-            "LGBTQ",
-            "Reproductive Issues"]
-    for essential in topics:
+    #Push data into database
+    for essential in topics.keys():
         for x in range(10):
             try:
-                with open(essential + '.json') as f:
-                    data = json.load(f)
-                    data_article = data["articles"][x]
-                    arg1 = (essential, data_article['title'])
-                    url = data_article['url']
-                    # check = urllib.urlopen(url)
-                    # if check.getcode() == 404:
-                    #     x += 1
-                    #     data_article = data["articles"][x]
-                    #     arg1 = (essential, data_article['title'])
-                    #     cursor.execute(sql_insert_essential_Query, arg1)
-                    #     url = data_article['url']
-                    #     print("Alternative article: " + url)
-                    article = Article(url)
-                    article.download()
-                    article.parse()
-                    # y = article.parse()
-                    # print(y)
-                    cursor.execute(sql_insert_essential_Query, arg1)
-                    content = str(article.text)
-                    content = content.replace('Advertisement', '')
-                    arg2 = (data_article['title'], content)
-                    #print(content)
-                    #print(arg)
-                    if data_article['title'] not in dict.keys():
-                        cursor.execute(sql_insert_Query, arg2)
-                        dict[data_article['title']] = True
-                    print(url)
+                data = dataList[essential]
+                data_article = data["articles"][x]
+                arg1 = (essential, data_article['title'])
+                url = data_article['url']
+
+                article = Article(url)
+                article.download()
+                article.parse()
+
+                cursor.execute(sql_insert_essential_Query, arg1)
+                content = str(article.text)
+                content = content.replace('Advertisement', '')
+                arg2 = (data_article['title'], content)
+                if data_article['title'] not in duplicate.keys():
+                    cursor.execute(sql_insert_Query, arg2)
+                    duplicate[data_article['title']] = True
+                print(url)
+
             except newspaper.article.ArticleException as err:
                 print("Couldn't download article")
                 pass
